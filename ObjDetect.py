ddafbf271b1dfd0042d54d1a7a0777a78a9162a4
theonionseed <tri@jitterware.com>
2020-05-04 16:24:24 -0400

    trying to run obj detection, we need to downgrade to python 3.6

--- ObjDetect.py
+++ ObjDetect.py
@@ -7,3 +7,17 @@ Problem: We need to classify the individual objects in an image before coloring.
 Reference: https://towardsdatascience.com/object-detection-with-10-lines-of-code-d6cb4d86f606
 
 '''
+
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
+
+for eachObject in detections:
+    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
