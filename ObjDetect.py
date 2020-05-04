d64d6d017a5610ff63403e63747a7700025f3187
theonionseed <tri@jitterware.com>
2020-05-04 16:14:01 -0400

    track h5 files

---
+++ ObjDetect.py
@@ -0,0 +1,9 @@
+'''
+Object Detection
+
+Problem: We need to classify the individual objects in an image before coloring.
+         This is so that we can choose training datasets that are closer related tot he objects within the input photo.
+
+Reference: https://towardsdatascience.com/object-detection-with-10-lines-of-code-d6cb4d86f606
+
+'''
