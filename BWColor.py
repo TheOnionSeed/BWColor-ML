76c031509ac4a93aad2883b031d9852bcde8b554
theonionseed <tri@jitterware.com>
2020-05-04 14:44:09 -0400

    add training ds

--- BWColor.py
+++ BWColor.py
@@ -1,3 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+
+IMAGE = "soldiers_1941.jpg"
+
+prototxt = "./model/colorization_deploy_v2.prototxt"
+model = "./model/colorization_release_v2.caffemodel"
+points = "./model/pts_in_hull.npy"
+image =  "./input_images/"+IMAGE
+
+net = cv2.dnn.readNetFromCaffe(prototxt, model)
+pts = np.load(points)
