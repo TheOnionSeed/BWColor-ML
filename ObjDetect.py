7fe7f013abe989ca700189482236c090c7dc9e3f
theonionseed <tri@jitterware.com>
2020-05-06 09:51:52 -0400

    tesla works too!

--- ObjDetect.py
+++ ObjDetect.py
@@ -21,7 +21,7 @@ detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
 detector.setModelPath( os.path.join(execution_path , model+"resnet50_coco_best_v2.0.1.h5"))
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , image+"street.jpeg"), output_image_path=os.path.join(execution_path , image+"imagenew.jpg"))
+detections,extracted_images  = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , image+"tesla_1883.jpg"), output_image_path=os.path.join(execution_path , image+"imagenew.jpg"), extract_detected_objects=True)
 
 for eachObject in detections:
     print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
